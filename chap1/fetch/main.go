6b7876b187816b4be5f00c8bf1d02f175ccec696
viiku <vivekvikkusingh@gmail.com>
2024-11-08 00:48:06 +0530

    fetch url content

---
+++ chap1/fetch/main.go
@@ -0,0 +1,28 @@
+// fetch the content of each specified URL and prints it as uninterpreted text;
+// it’s inspired by the invaluable utility curl
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// fmt.Printf("%s", url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		}
+		fmt.Printf("%s", b)
+	}
+}
